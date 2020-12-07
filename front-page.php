e7a7bcfe2f9b8611884f604113ad47791c04ee22
ShunyaSuzuki <anyushu2017@gmail.com>
2020-12-07 13:15:49 +0900

    update

--- front-page.php
+++ front-page.php
@@ -1,45 +1,56 @@
 <?php
 // header
 get_header(); ?>
+
 <?php
 // slides
 get_template_part('template/home/slide'); ?>
 
 <section class="sec pt-0">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8">
-        <h2 class="heading__h2">キャッチコピー<br>キャッチコピーキャッチコピー(25文字以内)</h2>
-        <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。<br>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、(50文字以内)</p>
-      </div>
-      <div class="col-lg-4">
-        <div class="review-home bg-light p-4 text-center">
-          <p class="review-home__count">4</p>
-          <p class="review-home__stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="far fa-star"></i>
-          </p>
-          <p class="review-home__link">
-            <a href="#">口コミ50件</a>
-          </p>
-        </div>
-      </div>
-    </div>
-    <!-- .row -->
-    <div class="appeal-point p-3 border my-md-5 my-4">
-      <h2 class="heading__h2 text-center">アピールポイント</h2>
-      <ul class="m-0 list__check">
-        <li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
-        <li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
-        <li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
-      </ul>
-    </div>
-    <!-- .appeal-point -->
-    <?php get_template_part('template/cta/buttons') ?>
-  </div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8">
+<h2 class="heading__h2">キャッチコピー<br>キャッチコピーキャッチコピー(25文字以内)</h2>
+<p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。<br>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、(50文字以内)</p>
+</div>
+<div class="col-lg-4">
+<div class="review-home bg-light p-4 text-center">
+<p class="review-home__count">4</p>
+<p class="review-home__stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="far fa-star"></i>
+</p>
+<p class="review-home__link">
+<a href="#">口コミ50件</a>
+</p>
+</div>
+</div>
+</div>
+<!-- .row -->
+<div class="appeal-point p-3 border my-md-5 my-4">
+<h2 class="heading__h2 text-center">アピールポイント</h2>
+<ul class="m-0 list__check">
+<li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
+<li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
+<li>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。(25文字以内)</li>
+</ul>
+</div>
+<!-- .appeal-point -->
+
+<?php
+// cta
+get_template_part('template/cta/buttons') ?>
+
+</div>
+</section>
+
+<section class="sec">
+<div class="container">
+<h2 class="heading__h2">基本情報<span class="text-body">(クリックで該当情報を確認できます)</span></h2>
+</div>
 </section>
 
 <?php
--- template/cta/buttons.php
+++ template/cta/buttons.php
@@ -1,14 +1,18 @@
 <div class="cta__btns">
-  <h2 class="heading__h2">まずはお気軽にご相談ください</h2>
-  <div class="cta__btns__list">
-    <a href="#" class="tel btn">
-      <span>0120-00-0000</span>
-    </a>
-    <a href="#" class="form btn">
-      <span>ご相談フォーム</span>
-    </a>
-    <a href="#" class="line btn">
-      <span>LINEで相談する</span>
-    </a>
-  </div>
+<h2 class="heading__h2">まずはお気軽にご相談ください</h2>
+<div class="cta__btns__list">
+<a href="#" class="tel btn">
+<i class="fas fa-phone-volume" style="font-size:2rem"></i>
+<span>0120-00-0000</span>
+</a>
+<a href="#" class="form btn">
+<i class="fas fa-envelope" style="font-size:2rem"></i>
+<span>ご相談フォーム</span>
+</a>
+<a href="#" class="line btn">
+<i class="fab fa-line" style="font-size:2rem"></i>
+<span>LINEで相談する</span>
+</a>
 </div>
+</div>
+<!-- .cta__btns -->
