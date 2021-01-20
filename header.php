2fa99bcc78017980ca9fc66606c65570889d69a1
Anyushu <suzuki@lamp.jp>
2021-01-20 18:31:44 +0900

    update

--- header.php
+++ header.php
@@ -4,6 +4,17 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <?php wp_head(); ?>
+
+<?php if (get_theme_mod('body_color')): ?>
+<style>body {color: <?php echo get_theme_mod('body_color'); ?>;}</style>
+<?php endif; ?>
+<?php if (get_theme_mod('primary_color')): ?>
+<style>.page-item.active .page-link,.copyright,.badge-area.active,.cta__btns__list .btn.form,.home-slide .next-arrow, .home-slide .prev-arrow,.btn-primary {background-color: <?php echo get_theme_mod('primary_color'); ?> !important;border-color: <?php echo get_theme_mod('primary_color'); ?> !important;}.post-header .meta .cat,.step-home__inner .number,.review-home__box--header .icon,.post-list__item:after,.post-list__item .meta .cat {border-color: <?php echo get_theme_mod('primary_color'); ?> !important;}.post-header .meta .cat,.page--title h2,.drawer-menu .page_item>ul>li>a:before,.step-home__inner .number span,.badge-area,.table th, table th,.post-inner h2,.post-list__item .meta .cat,.cta__btns .heading__h2:after, .cta__btns .heading__h2:before,.heading__h2,.text-primary {color: <?php echo get_theme_mod('primary_color'); ?> !important;}.page-link,.page-link:hover,a {color: <?php echo get_theme_mod('primary_color'); ?>;}.drawer-menu .page_item>a:before, .drawer-menu .page_item>span:before {border-bottom: .7rem solid <?php echo get_theme_mod('primary_color'); ?>;}</style>
+<?php endif; ?>
+<?php if (get_theme_mod('secondary_color')): ?>
+<style>.cta__btns__list .btn.tel {background-color: <?php echo get_theme_mod('secondary_color'); ?> !important;border-color: <?php echo get_theme_mod('secondary_color'); ?> !important;}</style>
+<?php endif; ?>
+
 <?php
 // headタグ出力
 if (get_option('tag-head')) {
@@ -34,7 +45,7 @@ if (get_option('tag-body-start')) {
 </h1>
 <!-- .logo -->
 <?php if (get_option('company-tel')): ?>
-<a href="tel:<?php echo get_option('company-tel'); ?>" class="tel">
+<a href="tel:<?php echo get_option('company-tel'); ?>" class="tel text-decoration-none">
 <i class="fas fa-phone-volume text-primary"></i>
 <span><?php echo get_option('company-tel'); ?></span>
 </a>
